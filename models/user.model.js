fa91d3f77f21fef5f4ed4f1b59e7b7de27113fe0
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-09 11:07:19 +0200

    fix : main category id on user model

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,11 @@ const {model, Schema} = require('mongoose')
 
 const userSchema = new Schema({
     username: String,
-    password: String
+    password: String,
+    mainCategoryId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    }
 })
 
 module.exports = model('User', userSchema)
