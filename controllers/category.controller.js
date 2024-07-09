263b78174bc0207c03ae304300872a1109352355
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-09 11:19:05 +0200

    feat: read categories function

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -9,7 +9,10 @@ module.exports = {
             .then(() => res.status(201).send('Category created.'))
             .catch(err => res.status(500).send('Error creating category.'));
     },
-    async read(req, res){},
+    async read(req, res){
+        const categories = await categoryModel.find({ user: req.user._id });
+        res.status(200).send(categories);
+    },
     async update(req, res){},
     async delete(req, res){}
 }
