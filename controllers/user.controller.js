44766ea9006e9e61fa6314536cde40b4711206a5
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-09 11:05:23 +0200

    fix: jwt expire 1h in login function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -29,7 +29,7 @@ module.exports = {
         const validPassword = bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).send('Invalid password.');
         // create token
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
         return res.status(200).send({ token, message : 'Logged in.'});
     }
 }
