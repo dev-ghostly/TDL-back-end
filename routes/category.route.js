c95ce0cba9249e0717c05f351a42839f094db813
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 04:04:42 +0200

    feat: add verifyToken to category routes

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,10 +1,11 @@
 const categoryController = require('../controllers/category.controller');
 const express = require('express');
 const router = express.Router();
+const verifyToken = require('../middleware/verifyToken.middleware');
 
-router.post('/', categoryController.create);
-router.get('/', categoryController.read);
-router.put('/:id', categoryController.update);
-router.delete('/:id', categoryController.delete);
+router.post('/', verifyToken, categoryController.create);
+router.get('/', verifyToken, categoryController.read);
+router.put('/:id', verifyToken, categoryController.update);
+router.delete('/:id', verifyToken, categoryController.delete);
 
 module.exports = router;
