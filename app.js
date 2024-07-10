1972f0d116b957842cb1cfda85b2e3bcb3241696
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 08:42:06 +0200

    feat: tasks router on app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 app.use('/api/users', require('./routes/user.route'));
 app.use('/api/categories', require('./routes/category.route'));
+app.use('/api/tasks', require('./routes/task.route'));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
