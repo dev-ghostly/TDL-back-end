9ffa7abf9f60ca1540f58cd069375ef8bba72f94
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 16:13:36 +0200

    fix: create app to avoid adress in use

---
+++ tests/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.use(express.json());
+
+app.use('/api/users', require('../routes/user.route'));
+app.use('/api/categories', require('../routes/category.route'));
+app.use('/api/tasks', require('../routes/task.route'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+module.exports = app;
