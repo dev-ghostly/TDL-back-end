c282b62c0fc8493139f1cbbe467b8ddedcfce397
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 14:36:54 +0200

    test: should create category

---
+++ tests/categories.test.js
@@ -0,0 +1,28 @@
+const app = require('../app');
+const request = require('supertest');
+
+var category = ''
+
+describe('CREATE /api/categories', () => {
+    test("It should create a category", async () => {
+        // connect and get the token
+        const response = await request(app).post('/api/users/login').send({username : 'test', password : 'test'});
+        const token = response.body.token;
+        // create a task
+        const response2 = await request(app).post('/api/categories').send({name : 'test'}).set('Authorization', 'Bearer ' + token);
+        category = response2.body._id;
+        expect(response2.statusCode).toBe(201);
+    })
+})
+
+describe('READ /api/categories', () => {
+
+})
+
+describe('UPDATE /api/categories', () => {
+
+})
+
+describe('DELETE /api/categories', () => {
+
+})
