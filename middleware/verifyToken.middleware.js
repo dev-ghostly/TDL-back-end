f83dd22db8e775e9e76a567ed4e5a5fe21396bf9
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 04:05:09 +0200

    fix: verify toekn bearer and fix env

--- middleware/verifyToken.middleware.js
+++ middleware/verifyToken.middleware.js
@@ -2,10 +2,11 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-    const token = req.header('Authorization');
+    // token bearer
+    const token = req.header('Authorization').split('Bearer ')[1];
     if (!token) return res.status(401).send('Access denied.');
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = decoded;
         next();
     } catch (err) {
