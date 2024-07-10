75b22404f2f7d1869884798a99ff4f32bd5d8131
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 09:22:18 +0200

    feat: login test

--- tests/users.test.js
+++ tests/users.test.js
@@ -4,12 +4,20 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(express.json());
 
-app.use('/api/users', require('./routes/user.route'));
+app.use('/api/users', require('../routes/user.route'));
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000);
+
+const request = require('supertest');
+
+describe('GET /api/users', () => {
+    test('It should return a JWT', async () => {
+        const response = await request(app).post('/api/users/login').send({username : 'test', password : 'test'});
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
+    });
+});
