52c3530a726a67686e29768457be17cf839d4f74
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-10 03:38:50 +0200

    feat: category route on app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use(express.json());
 
 app.use('/api/users', require('./routes/user.route'));
+app.use('/api/categories', require('./routes/category.route'));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
