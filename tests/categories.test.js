caac105d020b352de3ba2fcedba1aa159670d702
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-11 03:57:19 +0200

    test: Try to delete a non existing category

--- tests/categories.test.js
+++ tests/categories.test.js
@@ -87,4 +87,12 @@ describe('DELETE /api/categories', () => {
         const response = (await request(app).delete('/api/categories/' + category).set('Authorization', 'Bearer ' + 'wrong'))
         expect(response.statusCode).toBe(400);
     })
+    test("Try to delete a non existing category", async () => {
+        // connect and get the token
+        const response = await request(app).post('/api/users/login').send({username : 'test', password : 'test'});
+        const token = response.body.token;
+        // delete the category
+        const response2 = await request(app).delete('/api/categories/' + category).set('Authorization', 'Bearer ' + token);
+        expect(response2.statusCode).toBe(404);
+    })
 })
