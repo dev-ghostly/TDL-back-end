be41150f86ff888a44acf56ae125e43156b6fb76
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-07-11 04:05:46 +0200

    fix: delete console.log

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -23,7 +23,6 @@ module.exports = {
     },
     async delete(req, res){
         const category = await categoryModel.findOne({ _id: req.params.id });
-        console.log(category);
         if (!category) return res.status(404).send('Category not found.');
         category.deleteOne()
             .then(() => res.status(200).send('Category deleted.'))
